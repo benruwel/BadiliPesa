f4c5a449d489dd36ef496eb57103c7abc6bd6d62
Ben Ruwel <benruwel21@gmail.com>
2020-08-01 12:22:06 +0300

    add CurrenciesArrayAdapter to enable Recycle view of data list

--- app/src/main/java/com/moringa/badilipesa/CurrenciesActivity.java
+++ app/src/main/java/com/moringa/badilipesa/CurrenciesActivity.java
@@ -2,6 +2,7 @@ package com.moringa.badilipesa;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,31 @@ import butterknife.ButterKnife;
 
 public class CurrenciesActivity extends AppCompatActivity {
 
+    public String[] supportedCurrencies = new String[] {
+            "AED: 3.67295",
+            "AFN: 76.7",
+            "ALL: 105.3",
+            "AMD: 485.33",
+            "ANG: 1.795763",
+            "AOA: 576.975",
+            "ARS: 72.3764",
+            "AUD: 1.399805",
+            "AWG: 1.8",
+            "AZN: 1.7",
+            "BAM: 1.651305",
+            "BBD: 2.003622",
+            "BDT: 84.82762",
+            "BGN: 1.653791",
+            "BHD: 0.3769837",
+            "BIF: 1927.5",
+            "BMD: 1",
+            "BND: 1.372204",
+            "BOB: 6.913053",
+            "BRL: 5.22265",
+            "BSD: 1.00046",
+            "BTN: 74.735",
+    };
+
     @BindView(R.id.usernameTextView)
     TextView mUsernameTextView;
 
@@ -19,6 +45,8 @@ public class CurrenciesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.currencies);
         ButterKnife.bind(this);
+
+        ArrayAdapter currencies = new CurrenciesArrayAdapter(this, android.R.layout.simple_list_item_1 ,supportedCurrencies);
 
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
---
+++ app/src/main/java/com/moringa/badilipesa/CurrenciesArrayAdapter.java
@@ -0,0 +1,28 @@
+package com.moringa.badilipesa;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+public class CurrenciesArrayAdapter extends ArrayAdapter {
+
+    private Context mContext;
+    private String[] mSupportedCurrencies;
+
+    public CurrenciesArrayAdapter(Context mContext,int resource, String[] mSupportedCurrencies) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mSupportedCurrencies = mSupportedCurrencies;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mSupportedCurrencies[position];
+    }
+
+    @Override
+    public int getCount() {
+        return mSupportedCurrencies.length;
+    }
+}
