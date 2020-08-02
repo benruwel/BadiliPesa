61557c68a886dde845da5fe69e1ed793341990db
Ben Ruwel <benruwel21@gmail.com>
2020-08-02 11:49:43 +0300

    add passing tests for MainActivity class

---
+++ app/src/test/java/com/moringa/badilipesa/MainActivityTest.java
@@ -0,0 +1,39 @@
+package com.moringa.badilipesa;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView introText = activity.findViewById(R.id.textView3);
+        assertTrue("...the currency converter that you urgently needed".equals(introText.getText().toString()));
+    }
+
+    @Test
+    public void currenciesActivityStarted() {
+        activity.findViewById(R.id.buttonLogin).performClick();
+        Intent expectedIntent = new Intent(activity, CurrenciesActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
