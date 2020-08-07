343fe3140ec1928829b48954ed0032dfe8b4bed4
Ben Ruwel <benruwel21@gmail.com>
2020-08-07 16:40:11 +0300

    Add Constants class to hold api key and  base url

---
+++ app/src/main/java/com/moringa/badilipesa/util/Constants.java
@@ -0,0 +1,6 @@
+package com.moringa.badilipesa.util;
+
+public class Constants {
+    public static final String YELP_BASE_URL = "https://v1.nocodeapi.com/benruwel/cx/";
+    public static final String YELP_API_KEY = BuildConfig.CURRENCYEX_API_KEY;
+}
