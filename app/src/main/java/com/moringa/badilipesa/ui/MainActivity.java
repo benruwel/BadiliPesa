4bc8383eaa2d97ab5f362f718e27079dc53095f3
Ben Ruwel <benruwel21@gmail.com>
2020-08-14 17:04:03 +0300

    Redesign the main activity

--- app/src/main/java/com/moringa/badilipesa/ui/MainActivity.java
+++ app/src/main/java/com/moringa/badilipesa/ui/MainActivity.java
@@ -14,11 +14,11 @@ import android.os.Bundle;
 
 import com.moringa.badilipesa.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.buttonLogin) Button mButtonLogin;
-    @BindView(R.id.usernameText) EditText mUsernameText;
-    @BindView(R.id.currencySymbolInput) EditText mCurrencySymbol;
+//    @BindView(R.id.buttonLogin) Button mButtonLogin;
+//    @BindView(R.id.usernameText) EditText mUsernameText;
+//    @BindView(R.id.currencySymbolInput) EditText mCurrencySymbol;
 
 
     @Override
@@ -27,18 +27,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mButtonLogin.setOnClickListener(this);
+//        mButtonLogin.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        if(view == mButtonLogin) {
-            String username = mUsernameText.getText().toString();
-            String currencySymbol = mCurrencySymbol.getText().toString();
-            Intent intent = new Intent(MainActivity.this, RelatedCurrenciesListActivity.class);
-            intent.putExtra("username", username);
-            intent.putExtra("currencySymbol", currencySymbol);
-            startActivity(intent);
-        }
-    }
+//    @Override
+//    public void onClick(View view) {
+//        if(view == mButtonLogin) {
+//            String username = mUsernameText.getText().toString();
+//            String currencySymbol = mCurrencySymbol.getText().toString();
+//            Intent intent = new Intent(MainActivity.this, RelatedCurrenciesListActivity.class);
+//            intent.putExtra("username", username);
+//            intent.putExtra("currencySymbol", currencySymbol);
+//            startActivity(intent);
+//        }
+//    }
 }
