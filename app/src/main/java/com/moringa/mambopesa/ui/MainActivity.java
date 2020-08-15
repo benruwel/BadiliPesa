caf9b38147570e34f328fa9fa04a44756a18bc21
Ben Ruwel <benruwel21@gmail.com>
2020-08-15 17:46:24 +0300

    Add popup menu in Main Activity to hold the logout and later the profile events

--- app/src/main/java/com/moringa/mambopesa/ui/MainActivity.java
+++ app/src/main/java/com/moringa/mambopesa/ui/MainActivity.java
@@ -2,18 +2,25 @@ package com.moringa.mambopesa.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import com.moringa.mambopesa.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-//    @BindView(R.id.buttonLogin) Button mButtonLogin;
-//    @BindView(R.id.usernameText) EditText mUsernameText;
-//    @BindView(R.id.currencySymbolInput) EditText mCurrencySymbol;
+    @BindView(R.id.profileImage)
+    ImageView mProfileImage;
 
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,11 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 //        mButtonLogin.setOnClickListener(this);
+        mProfileImage.setOnClickListener(this);
     }
 
-//    @Override
-//    public void onClick(View view) {
+    @Override
+    public void onClick(View view) {
 //        if(view == mButtonLogin) {
 //            String username = mUsernameText.getText().toString();
 //            String currencySymbol = mCurrencySymbol.getText().toString();
@@ -34,5 +42,21 @@ public class MainActivity extends AppCompatActivity {
 //            intent.putExtra("currencySymbol", currencySymbol);
 //            startActivity(intent);
 //        }
-//    }
+
+        if(view == mProfileImage) {
+            PopupMenu popupMenu = new PopupMenu(MainActivity.this, mProfileImage);
+            //inflating the popup using xml
+            popupMenu.getMenuInflater().inflate(R.menu.menu_logout, popupMenu.getMenu());
+
+            popupMenu.show();
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    //we use a switch statement to handle the diff events of the various menu items
+                    //TODO add logout logic
+                    return true;
+                }
+            });
+        }
+    }
 }
