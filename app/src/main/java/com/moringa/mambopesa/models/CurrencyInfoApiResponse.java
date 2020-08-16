d1bbae4f61f4c4cd1201be1deb08776d26e25eb6
Ben Ruwel <benruwel21@gmail.com>
2020-08-16 16:32:19 +0300

    Generate POJOs for the CurrencyInfoApiResponse class and its nested classes

---
+++ app/src/main/java/com/moringa/mambopesa/models/CurrencyInfoApiResponse.java
@@ -0,0 +1,90 @@
+
+package com.moringa.mambopesa.models;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CurrencyInfoApiResponse {
+
+    @SerializedName("status")
+    @Expose
+    private Boolean status;
+    @SerializedName("code")
+    @Expose
+    private Integer code;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
+    @SerializedName("response")
+    @Expose
+    private List<Response> response = null;
+    @SerializedName("info")
+    @Expose
+    private Info info;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public CurrencyInfoApiResponse() {
+    }
+
+    /**
+     * 
+     * @param msg
+     * @param code
+     * @param response
+     * @param status
+     * @param info
+     */
+    public CurrencyInfoApiResponse(Boolean status, Integer code, String msg, List<Response> response, Info info) {
+        super();
+        this.status = status;
+        this.code = code;
+        this.msg = msg;
+        this.response = response;
+        this.info = info;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<Response> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<Response> response) {
+        this.response = response;
+    }
+
+    public Info getInfo() {
+        return info;
+    }
+
+    public void setInfo(Info info) {
+        this.info = info;
+    }
+
+}
---
+++ app/src/main/java/com/moringa/mambopesa/models/Info.java
@@ -0,0 +1,50 @@
+
+package com.moringa.mambopesa.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Info {
+
+    @SerializedName("server_time")
+    @Expose
+    private String serverTime;
+    @SerializedName("credit_count")
+    @Expose
+    private Integer creditCount;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Info() {
+    }
+
+    /**
+     * 
+     * @param creditCount
+     * @param serverTime
+     */
+    public Info(String serverTime, Integer creditCount) {
+        super();
+        this.serverTime = serverTime;
+        this.creditCount = creditCount;
+    }
+
+    public String getServerTime() {
+        return serverTime;
+    }
+
+    public void setServerTime(String serverTime) {
+        this.serverTime = serverTime;
+    }
+
+    public Integer getCreditCount() {
+        return creditCount;
+    }
+
+    public void setCreditCount(Integer creditCount) {
+        this.creditCount = creditCount;
+    }
+
+}
