a5ad9f61b77e06ba1ded3f7f202df4c7deb5b2d0
Ben Ruwel <benruwel21@gmail.com>
2020-08-21 12:49:09 +0300

    Generate AddExpense activity class and its layout files

---
+++ app/src/main/java/com/moringa/mambopesa/ui/AddExpenseActivity.java
@@ -0,0 +1,16 @@
+package com.moringa.mambopesa.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringa.mambopesa.R;
+
+import android.os.Bundle;
+
+public class AddExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}
