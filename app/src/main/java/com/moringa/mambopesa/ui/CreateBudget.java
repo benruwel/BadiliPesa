bd32586974b315df734a67b08c4927a3c85c7a25
Ben Ruwel <benruwel21@gmail.com>
2020-08-21 11:32:51 +0300

    Generate CreateBudget activity class and its layout

---
+++ app/src/main/java/com/moringa/mambopesa/ui/CreateBudget.java
@@ -0,0 +1,15 @@
+package com.moringa.mambopesa.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.moringa.mambopesa.R;
+
+import android.os.Bundle;
+
+public class CreateBudget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_budget);
+    }
+}
--- app/src/main/java/com/moringa/mambopesa/ui/MainActivity.java
+++ app/src/main/java/com/moringa/mambopesa/ui/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //method to logout user current session
     private void logOutUser() {
         FirebaseAuth.getInstance().signOut();
-        //TODO change this to login activity after debugging
+
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
